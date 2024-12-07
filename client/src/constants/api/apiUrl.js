43030f835cd9676820e047a246388a83900b9225
kemaltt <ttnckemal@gmail.com>
2024-12-07 23:42:33 +0100

    Aktualisiere die BASE_URL für die Produktionsumgebung, um den API-Pfad korrekt zu reflektieren

--- client/src/constants/api/apiUrl.js
+++ client/src/constants/api/apiUrl.js
@@ -1,4 +1,4 @@
 // Desc: API URL
-export const BASE_URL = process.env.REACT_APP_API_PATH === "production" ? 'https://shoppingapp-mern.onrender.com' : 'http://localhost:9090/api'
+export const BASE_URL = process.env.REACT_APP_API_PATH === "production" ? 'https://shoppingapp-mern.onrender.com/api' : 'http://localhost:9090/api'
 
 
