6cce5d165234895b0a749c5b9b8d87faaf3036ba
kemaltt <ttnckemal@gmail.com>
2024-12-13 23:02:53 +0100

    Aktualisiere die BASE_URL für die Produktionsumgebung auf die neue API-Adresse

--- client/src/constants/api/apiUrl.js
+++ client/src/constants/api/apiUrl.js
@@ -1,5 +1,5 @@
 // Desc: API URL
-export const BASE_URL = process.env.REACT_APP_API_PATH === "production" ? 'https://shoppingapp-mern.onrender.com/api' : 'http://localhost:9090/api'
-// export const BASE_URL = process.env.REACT_APP_API_PATH === "production" ? 'https://shopping-app-mern-indol.vercel.app/api' : 'http://localhost:9090/api'
+// export const BASE_URL = process.env.REACT_APP_API_PATH === "production" ? 'https://shoppingapp-mern.onrender.com/api' : 'http://localhost:9090/api'
+export const BASE_URL = process.env.REACT_APP_API_PATH === "production" ? 'https://shopping-app-mern-indol.vercel.app/api' : 'http://localhost:9090/api'
 
 
